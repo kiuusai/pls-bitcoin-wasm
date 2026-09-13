a90edeae68e98379e2616594e51b16c60c6d6399
kiuusai <kiuusai@protonmail.com>
2026-09-13 00:32:55 -0300

    Using init.sh only to configure Cargo

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-# This script is strictly for first setup in your devcontainer
-# or your development environment.
-# Ensure you have asdf installed if you want to run it outside devcontainer
+# This script it's strictly to being used to configure Devcontainers.
+# Do not try to run it manually without understanding what are you doing.
 
 set -e
-
-FILENAME="$(realpath "${BASH_SOURCE[0]}")"
-DIRNAME="$(dirname "${FILENAME}")"
-PROJECT_DIR="$(realpath "${DIRNAME}/..")"
-
-install_plugins() {
-  plugins=(
-    rust
-    nodejs
-    golang
-  )
-
-  for plugin in "${plugins[@]}";
-  do
-    asdf plugin add "${plugin}"
-  done
-}
-
-install_asdf_deps() {
-  cd "${PROJECT_DIR}"
-
-  asdf install
-}
 
 install_cargo_deps() {
   sudo apt update && sudo apt install build-essential clang
@@ -44,17 +20,8 @@ install_cargo_deps() {
   cargo install --locked "${tools[@]}"
 }
 
-install_npm_deps() {
-  cd "${PROJECT_DIR}"
-
-  npm install
-}
-
 main() {
-  (install_plugins)
-  (install_asdf_deps)
   (install_cargo_deps)
-  (install_npm_deps)
 }
 
 main
