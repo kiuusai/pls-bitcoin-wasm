b9866e28411b7023b5c65db46f23097667f32fe3
kiuusai <kiuusai@protonmail.com>
2026-09-15 13:05:25 -0300

    Optimizing build-ts script

--- scripts/build-ts.sh
+++ scripts/build-ts.sh
@@ -9,13 +9,31 @@ PROJECT_DIR="$(realpath "${DIRNAME}/..")"
 TARGET="wasm32-unknown-unknown"
 
 WASM_SOURCE="target/${TARGET}/release/pls_bitcoin_wasm.wasm"
+WIT_SOURCE="wit/multisig.wit"
 OUTPUT="ts/bitcoin"
 
 build_ts() {
   cd "${PROJECT_DIR}"
 
-  jco transpile -mO "${WASM_SOURCE}" -o "${OUTPUT}"
-  jco types wit/multisig.wit --name pls_bitcoin_wasm -o "${OUTPUT}"
+  transpile_args=(
+    --no-typescript
+    --no-component-error-wrapping
+    --no-namespaced-exports
+    --minify
+    --optimize
+  )
+
+  general_args=(
+    --name pls_bitcoin_wasm
+  )
+
+  jco transpile \
+    "${transpile_args[@]}" \
+    "${general_args[@]}" \
+    "${WASM_SOURCE}" -o "${OUTPUT}"
+  jco types \
+    "${general_args[@]}" \
+    "${WIT_SOURCE}" -o "${OUTPUT}"
 }
 
 (build_ts)
