0f8eb47e2fb932401ac7cb5c2ee9df7cf193097d
kiuusai <kiuusai@protonmail.com>
2026-09-24 22:19:30 -0300

    Fixing ts index build logic

--- ts/build.js
+++ ts/build.js
@@ -1,13 +1,22 @@
-import { multisig } from "./bitcoin/pls_bitcoin_wasm.js";
 import ejs from "ejs";
 import fs from "fs";
 
-const properties = Object.keys(multisig);
-
 const versions = ["node", "web"];
 
-for (let version of versions) {
-  const result = await ejs.renderFile("./bitcoin/index.ejs", { version, properties });
+async function main() {
+  for (let version of versions) {
+    console.log(`Building index file for ${version} environment`);
 
-  fs.writeFileSync(`./bitcoin/index.${version}.js`, result);
+    // It fetches node every time just to get properties names
+    // Fetching web crashes build script and both versions entries are the same
+    const { multisig } = await import(`./bitcoin/pls_bitcoin_wasm_node.js`);
+
+    const properties = Object.keys(multisig);
+
+    const result = await ejs.renderFile("./bitcoin/index.ejs", { version, properties });
+
+    fs.writeFileSync(`./bitcoin/index.${version}.js`, result);
+  }
 }
+
+main();
